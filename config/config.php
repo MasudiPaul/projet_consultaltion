c2b2494a0700b4bbe0459d65ce02ac604f8f5492
Risasipaul <masudirisasipaul@.com>
2024-10-26 11:08:32 +0200

    bd

--- config/config.php
+++ config/config.php
@@ -8,5 +8,5 @@
     if(!defined('BD_SERVEUR')) define ('BD_SERVEUR','127.0.0.1');
     if(!defined('BD_UTILISATEUR')) define ('BD_UTILISATEUR','root');
     if(!defined('BD_PASSWORD')) define ('BD_PASSWORD','');
-    if(!defined('BD_NOM')) define ('BD_NOM','c2465120c_bd');
+    if(!defined('BD_NOM')) define ('BD_NOM','c2465120c_rmb_bd');
     if(!defined('BD_DSN')) define ('BD_DSN','mysql:host='.BD_SERVEUR.';dbname='.BD_NOM);
